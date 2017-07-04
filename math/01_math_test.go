3c37a533f5d4ca13996bcdf3e11e8d20102e45c1
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2017-07-04 17:00:05 +0800

    Added most of the Hacker's Delights' implementations

--- math/01_math_test.go
+++ math/01_math_test.go
@@ -133,14 +133,43 @@ func TestBitCountUint64Naive(t *testing.T) {
 	}
 }
 
+func TestBitCountUint(t *testing.T) {
+	bitCountUintFuncs := []struct {
+		name string
+		f    func(uint) uint
+	}{
+		{"BitCountUintGCCImpl", math.BitCountUintGCCImpl},
+		{"BitCountUintGCCImplSwitch", math.BitCountUintGCCImplSwitch},
+	}
+	gen := randomIntsInTheFirstPage()
+	for _, bitCountUintFunc := range bitCountUintFuncs {
+		for i := 0; i < 512; i++ {
+			n := uint(gen())
+			expected := math.BitCountUintNaive(n)
+			got := bitCountUintFunc.f(n)
+			if got != expected {
+				t.Errorf("%s(%d) = %d, expected %d", bitCountUintFunc.name, n, got, expected)
+			}
+		}
+	}
+}
+
 func TestBitCountUint32(t *testing.T) {
 	bitCountUint32Funcs := []struct {
 		name string
 		f    func(uint32) uint
 	}{
-		{"BitCountUint32Java", math.BitCountUint32Java},
 		{"BitCountUint32CallGCC", math.BitCountUint32CallGCC},
-		{"BitCountUint32GCCImpl", math.BitCountUint32GCCImpl},
+		{"BitCountUint32Pop0", math.BitCountUint32Pop0},
+		{"BitCountUint32Pop1", math.BitCountUint32Pop1},
+		{"BitCountUint32Pop1Alt", math.BitCountUint32Pop1Alt},
+		{"BitCountUint32Pop2", math.BitCountUint32Pop2},
+		{"BitCountUint32Pop2Alt", math.BitCountUint32Pop2Alt},
+		{"BitCountUint32Pop3", math.BitCountUint32Pop3},
+		{"BitCountUint32Pop4", math.BitCountUint32Pop4},
+		{"BitCountUint32Pop5", math.BitCountUint32Pop5},
+		{"BitCountUint32Pop5a", math.BitCountUint32Pop5a},
+		{"BitCountUint32Pop6", math.BitCountUint32Pop6},
 	}
 	gen := randomIntsInTheFirstPage()
 	for _, bitCountUint32Func := range bitCountUint32Funcs {
@@ -160,9 +189,9 @@ func TestBitCountUint64(t *testing.T) {
 		name string
 		f    func(uint64) uint
 	}{
-		{"BitCountUint64Java", math.BitCountUint64Java},
 		{"BitCountUint64CallGCC", math.BitCountUint64CallGCC},
-		{"BitCountUint64GCCImpl", math.BitCountUint64GCCImpl},
+		{"BitCountUint64Pop1", math.BitCountUint64Pop1},
+		{"BitCountUint64Pop1Alt", math.BitCountUint64Pop1Alt},
 	}
 	gen := randomIntsInTheFirstPage()
 	for _, bitCountUint64Func := range bitCountUint64Funcs {
@@ -177,12 +206,107 @@ func TestBitCountUint64(t *testing.T) {
 	}
 }
 
+// --- uint ---
+
+// BenchmarkBitCountUintNaive-8                      	10000000	       186 ns/op
+// BenchmarkBitCountUintGCCImpl-8                    	100000000	        12.2 ns/op
+// BenchmarkBitCountUintGCCImplSwitch-8              	100000000	        11.9 ns/op
+
+func benchmarkBitCountUint(b *testing.B, f func(uint) uint) {
+	gen := randomIntsInTheFirstPage()
+	for i, count := 0, b.N; i < count; i++ {
+		f(uint(gen()))
+	}
+}
+
+func BenchmarkBitCountUintNaive(b *testing.B) {
+	benchmarkBitCountUint(b, math.BitCountUintNaive)
+}
+
+func BenchmarkBitCountUintGCCImpl(b *testing.B) {
+	benchmarkBitCountUint(b, math.BitCountUintGCCImpl)
+}
+
+func BenchmarkBitCountUintGCCImplSwitch(b *testing.B) {
+	benchmarkBitCountUint(b, math.BitCountUintGCCImplSwitch)
+}
+
+// --- uint32 ---
+
+// BenchmarkBitCountUint32Naive-8                    	20000000	        98.2 ns/op
+// BenchmarkBitCountUint32CallGCC-8                  	20000000	        67.1 ns/op
+// BenchmarkBitCountUint32Pop0-8                     	100000000	        12.6 ns/op
+// BenchmarkBitCountUint32Pop1-8                     	100000000	        11.9 ns/op
+// BenchmarkBitCountUint32Pop1Alt-8                  	100000000	        11.7 ns/op
+// BenchmarkBitCountUint32Pop2-8                     	100000000	        12.3 ns/op
+// BenchmarkBitCountUint32Pop2Alt-8                  	100000000	        11.9 ns/op
+// BenchmarkBitCountUint32Pop3-8                     	100000000	        11.8 ns/op
+// BenchmarkBitCountUint32Pop4-8                     	50000000	        27.4 ns/op
+// BenchmarkBitCountUint32Pop5-8                     	10000000	       155 ns/op
+// BenchmarkBitCountUint32Pop5a-8                    	50000000	        28.8 ns/op
+// BenchmarkBitCountUint32Pop6-8                     	100000000	        11.7 ns/op
+
 func benchmarkBitCountUint32(b *testing.B, f func(uint32) uint) {
 	gen := randomIntsInTheFirstPage()
 	for i, count := 0, b.N; i < count; i++ {
 		f(uint32(gen()))
 	}
 }
+
+func BenchmarkBitCountUint32Naive(b *testing.B) {
+	benchmarkBitCountUint32(b, math.BitCountUint32Naive)
+}
+
+func BenchmarkBitCountUint32CallGCC(b *testing.B) {
+	benchmarkBitCountUint32(b, math.BitCountUint32CallGCC)
+}
+
+func BenchmarkBitCountUint32Pop0(b *testing.B) {
+	benchmarkBitCountUint32(b, math.BitCountUint32Pop0)
+}
+
+func BenchmarkBitCountUint32Pop1(b *testing.B) {
+	benchmarkBitCountUint32(b, math.BitCountUint32Pop1)
+}
+
+func BenchmarkBitCountUint32Pop1Alt(b *testing.B) {
+	benchmarkBitCountUint32(b, math.BitCountUint32Pop1Alt)
+}
+
+func BenchmarkBitCountUint32Pop2(b *testing.B) {
+	benchmarkBitCountUint32(b, math.BitCountUint32Pop2)
+}
+
+func BenchmarkBitCountUint32Pop2Alt(b *testing.B) {
+	benchmarkBitCountUint32(b, math.BitCountUint32Pop2Alt)
+}
+
+func BenchmarkBitCountUint32Pop3(b *testing.B) {
+	benchmarkBitCountUint32(b, math.BitCountUint32Pop3)
+}
+
+func BenchmarkBitCountUint32Pop4(b *testing.B) {
+	benchmarkBitCountUint32(b, math.BitCountUint32Pop4)
+}
+
+func BenchmarkBitCountUint32Pop5(b *testing.B) {
+	benchmarkBitCountUint32(b, math.BitCountUint32Pop5)
+}
+
+func BenchmarkBitCountUint32Pop5a(b *testing.B) {
+	benchmarkBitCountUint32(b, math.BitCountUint32Pop5a)
+}
+
+func BenchmarkBitCountUint32Pop6(b *testing.B) {
+	benchmarkBitCountUint32(b, math.BitCountUint32Pop6)
+}
+
+// --- uint64 ---
+
+// BenchmarkBitCountUint64Naive-8                    	10000000	       181 ns/op
+// BenchmarkBitCountUint64CallGCC-8                  	20000000	        66.0 ns/op
+// BenchmarkBitCountUint64Pop1-8                     	100000000	        12.6 ns/op
+// BenchmarkBitCountUint64Pop1Alt-8                  	100000000	        12.2 ns/op
 
 func benchmarkBitCountUint64(b *testing.B, f func(uint64) uint) {
 	gen := randomIntsInTheFirstPage()
@@ -191,36 +315,18 @@ func benchmarkBitCountUint64(b *testing.B, f func(uint64) uint) {
 	}
 }
 
-func BenchmarkBitCountUint32Naive(b *testing.B) {
-	benchmarkBitCountUint32(b, math.BitCountUint32Naive)
-}
-
 func BenchmarkBitCountUint64Naive(b *testing.B) {
 	benchmarkBitCountUint64(b, math.BitCountUint64Naive)
-}
-
-// java.lang.Integer#bitCount
-func BenchmarkBitCountUint32Java(b *testing.B) {
-	benchmarkBitCountUint32(b, math.BitCountUint32Java)
-}
-
-// java.lang.Long#bitCount
-func BenchmarkBitCountUint64Java(b *testing.B) {
-	benchmarkBitCountUint64(b, math.BitCountUint64Java)
-}
-
-func BenchmarkBitCountUint32CallGCC(b *testing.B) {
-	benchmarkBitCountUint32(b, math.BitCountUint32CallGCC)
 }
 
 func BenchmarkBitCountUint64CallGCC(b *testing.B) {
 	benchmarkBitCountUint64(b, math.BitCountUint64CallGCC)
 }
 
-func BenchmarkBitCountUint32GCCImpl(b *testing.B) {
-	benchmarkBitCountUint32(b, math.BitCountUint32GCCImpl)
+func BenchmarkBitCountUint64Pop1(b *testing.B) {
+	benchmarkBitCountUint64(b, math.BitCountUint64Pop1)
 }
 
-func BenchmarkBitCountUint64GCCImpl(b *testing.B) {
-	benchmarkBitCountUint64(b, math.BitCountUint64GCCImpl)
+func BenchmarkBitCountUint64Pop1Alt(b *testing.B) {
+	benchmarkBitCountUint64(b, math.BitCountUint64Pop1Alt)
 }
--- math/bitcount.go
+++ math/bitcount.go
@@ -1,5 +1,7 @@
 package math
 
+// // These functions calls the built-ins in GCC / clang.
+//
 // static inline int popcount(unsigned int x) {
 //     return __builtin_popcount(x);
 // }
@@ -12,6 +14,7 @@ package math
 //     return __builtin_popcountll(x);
 // }
 import "C"
+import "unsafe"
 
 // BitCountUintNaive returns the number of 1-bits in n.
 func BitCountUintNaive(n uint) uint {
@@ -49,53 +52,240 @@ func BitCountUint64Naive(n uint64) uint {
 	return count
 }
 
-// BitCountUint32Java return the number of 1-bits in n.
-func BitCountUint32Java(n uint32) uint {
-	n = n - ((n >> 1) & uint32(0x55555555))
-	n = (n & uint32(0x33333333)) + ((n >> 2) & uint32(0x33333333))
-	n = (n + (n >> 4)) & uint32(0x0f0f0f0f)
-	n = n + (n >> 8)
-	n = n + (n >> 16)
-	return uint(n & uint32(0x3f))
-}
-
-// BitCountUint64Java return the number of 1-bits in n.
-func BitCountUint64Java(n uint64) uint {
-	n = n - ((n >> 1) & uint64(0x5555555555555555))
-	n = (n & uint64(0x3333333333333333)) + ((n >> 2) & uint64(0x3333333333333333))
-	n = (n + (n >> 4)) & uint64(0x0f0f0f0f0f0f0f0f)
-	n = n + (n >> 8)
-	n = n + (n >> 16)
-	n = n + (n >> 32)
-	return uint(n & uint64(0x7f))
-}
-
-// BitCountUint32CallGCC return the number of 1-bits in n.
+// BitCountUint32CallGCC returns the number of 1-bits in n.
 func BitCountUint32CallGCC(n uint32) uint {
 	return uint(C.popcount(C.uint(n)))
 }
 
-// BitCountUint64CallGCC return the number of 1-bits in n.
+// BitCountUint64CallGCC returns the number of 1-bits in n.
 func BitCountUint64CallGCC(n uint64) uint {
 	return uint(C.popcountll(C.ulonglong(n)))
 }
 
-// BitCountUint32GCCImpl return the number of 1-bits in n.
-// Source: https://github.com/gcc-mirror/gcc/blob/master/libgcc/libgcc2.c#L840-L859
-// Source: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=36041#c8
-func BitCountUint32GCCImpl(n uint32) uint {
-	n = n - ((n >> 1) & uint32(0x55555555))
-	n = (n & uint32(0x33333333)) + ((n >> 2) & uint32(0x33333333))
-	n = (n + (n >> 4)) & uint32(0x0f0f0f0f)
-	return uint((n * uint32(0x01010101)) >> 24)
+const (
+	maxUint              = ^uint(0)
+	logSizeOfUintInBytes = maxUint>>8&1 + maxUint>>16&1 + maxUint>>32&1
+	sizeOfUintInBytes    = 1 << logSizeOfUintInBytes
+	sizeOfUintInBits     = sizeOfUintInBytes << 3
+)
+
+var (
+	bitMask55 uint
+	bitMask33 uint
+	bitMask0f uint
+	bitMask01 uint
+	bitShift  uint
+)
+
+func init() {
+	genBitMask := func(x uint) uint {
+		bitMask := x
+		shift := uint(8)
+		for i := uint(0); i < logSizeOfUintInBytes; i++ {
+			bitMask |= (bitMask << shift)
+			shift <<= 1
+		}
+		return bitMask
+	}
+	bitMask55 = genBitMask(0x55)
+	bitMask33 = genBitMask(0x33)
+	bitMask0f = genBitMask(0x0f)
+	bitMask01 = genBitMask(0x01)
+	bitShift = sizeOfUintInBits - 8
 }
 
-// BitCountUint64GCCImpl return the number of 1-bits in n.
+// BitCountUintGCCImpl returns the number of 1-bits in n.
 // Source: https://github.com/gcc-mirror/gcc/blob/master/libgcc/libgcc2.c#L840-L859
 // Source: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=36041#c8
-func BitCountUint64GCCImpl(n uint64) uint {
-	n = n - ((n >> 1) & uint64(0x5555555555555555))
-	n = (n & uint64(0x3333333333333333)) + ((n >> 2) & uint64(0x3333333333333333))
-	n = (n + (n >> 4)) & uint64(0x0f0f0f0f0f0f0f0f)
-	return uint((n * uint64(0x0101010101010101)) >> 56)
+func BitCountUintGCCImpl(n uint) uint {
+	n = n - ((n >> 1) & bitMask55)
+	n = (n & bitMask33) + ((n >> 2) & bitMask33)
+	n = (n + (n >> 4)) & bitMask0f
+	return (n * bitMask01) >> bitShift
+}
+
+// BitCountUintGCCImplSwitch returns the number of 1-bits in n.
+func BitCountUintGCCImplSwitch(n uint) uint {
+	switch sizeOfUintInBits {
+	case 32:
+		return BitCountUint32Pop1Alt(uint32(n))
+	case 64:
+		return BitCountUint64Pop1Alt(uint64(n))
+	default:
+		panic("uint is neither 32-bit nor 64-bit")
+	}
+}
+
+// BitCountUint32Pop0 returns the number of 1-bits in x.
+// Source: http://www.hackersdelight.org/hdcodetxt/pop.c.txt (pop0)
+func BitCountUint32Pop0(x uint32) uint {
+	x = (x & 0x55555555) + ((x >> 1) & 0x55555555)
+	x = (x & 0x33333333) + ((x >> 2) & 0x33333333)
+	x = (x & 0x0f0f0f0f) + ((x >> 4) & 0x0f0f0f0f)
+	x = (x & 0x00ff00ff) + ((x >> 8) & 0x00ff00ff)
+	x = (x & 0x0000ffff) + ((x >> 16) & 0x0000ffff)
+	return uint(x)
+}
+
+// BitCountUint32Pop1 returns the number of 1-bits in x.
+// Source: http://www.hackersdelight.org/hdcodetxt/pop.c.txt (pop1)
+// Source: java.lang.Integer#bitCount
+// Source: http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/8u40-b25/java/lang/Integer.java#Integer.bitCount%28int%29
+func BitCountUint32Pop1(x uint32) uint {
+	x = x - ((x >> 1) & 0x55555555)
+	x = (x & 0x33333333) + ((x >> 2) & 0x33333333)
+	x = (x + (x >> 4)) & 0x0f0f0f0f
+	x = x + (x >> 8)
+	x = x + (x >> 16)
+	return uint(x & 0x0000003f)
+}
+
+// BitCountUint64Pop1 returns the number of 1-bits in x.
+// Source: java.lang.Long#bitCount
+// Source: http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/8u40-b25/java/lang/Long.java#Long.bitCount%28long%29
+func BitCountUint64Pop1(x uint64) uint {
+	x = x - ((x >> 1) & 0x5555555555555555)
+	x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333)
+	x = (x + (x >> 4)) & 0x0f0f0f0f0f0f0f0f
+	x = x + (x >> 8)
+	x = x + (x >> 16)
+	x = x + (x >> 32)
+	return uint(x & 0x000000000000007f)
+}
+
+// BitCountUint32Pop1Alt returns the number of 1-bits in x.
+// Source: http://www.hackersdelight.org/hdcodetxt/pop.c.txt (pop1 + alternative)
+// Source: https://github.com/gcc-mirror/gcc/blob/master/libgcc/libgcc2.c#L840-L859
+// Source: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=36041#c8
+func BitCountUint32Pop1Alt(x uint32) uint {
+	x = x - ((x >> 1) & 0x55555555)
+	x = (x & 0x33333333) + ((x >> 2) & 0x33333333)
+	x = (x + (x >> 4)) & 0x0f0f0f0f
+	return uint((x * 0x01010101) >> 24)
+}
+
+// BitCountUint64Pop1Alt returns the number of 1-bits in x.
+// Source: http://www.hackersdelight.org/hdcodetxt/pop.c.txt (pop1 + alternative)
+// Source: https://github.com/gcc-mirror/gcc/blob/master/libgcc/libgcc2.c#L840-L859
+// Source: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=36041#c8
+func BitCountUint64Pop1Alt(x uint64) uint {
+	x = x - ((x >> 1) & 0x5555555555555555)
+	x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333)
+	x = (x + (x >> 4)) & 0x0f0f0f0f0f0f0f0f
+	return uint((x * 0x0101010101010101) >> 56)
+}
+
+// BitCountUint32Pop2 returns the number of 1-bits in x.
+// Source: http://www.hackersdelight.org/hdcodetxt/pop.c.txt (pop2)
+func BitCountUint32Pop2(x uint32) uint {
+	n := (x >> 1) & 033333333333 // Count bits in
+	x = x - n                    // each 3-bit
+	n = (n >> 1) & 033333333333  // field.
+	x = x - n
+	x = (x + (x >> 3)) & 030707070707 // 6-bit sums.
+	return uint(x % 63)               // Add 6-bit sums.
+}
+
+// BitCountUint32Pop2Alt returns the number of 1-bits in x.
+// Source: http://www.hackersdelight.org/hdcodetxt/pop.c.txt (pop2 + alternative)
+func BitCountUint32Pop2Alt(x uint32) uint {
+	n := (x >> 1) & 033333333333 // Count bits in
+	x = x - n                    // each 3-bit
+	n = (n >> 1) & 033333333333  // field.
+	x = x - n
+	x = (x + (x >> 3)) & 030707070707                 // 6-bit sums.
+	return uint(((x * 0404040404) >> 26) + (x >> 30)) // Add 6-bit sums.
+}
+
+// BitCountUint32Pop3 returns the number of 1-bits in x.
+// Source: http://www.hackersdelight.org/hdcodetxt/pop.c.txt (pop3)
+func BitCountUint32Pop3(x uint32) uint {
+	n := (x >> 1) & 0x77777777 // Count bits in
+	x = x - n                  // each 4-bit
+	n = (n >> 1) & 0x77777777  // field.
+	x = x - n
+	n = (n >> 1) & 0x77777777
+	x = x - n
+	x = (x + (x >> 4)) & 0x0f0f0f0f // Get byte sums.
+	x = x * 0x01010101              // Add the bytes.
+	return uint(x >> 24)
+}
+
+// BitCountUint32Pop4 returns the number of 1-bits in x.
+// Source: http://www.hackersdelight.org/hdcodetxt/pop.c.txt (pop4)
+func BitCountUint32Pop4(x uint32) uint {
+	n := uint(0)
+	for x != 0 {
+		n = n + 1
+		x = x & (x - 1)
+	}
+	return n
+}
+
+func rotate32(x uint32, n uint) uint32 {
+	if n > 63 {
+		panic("rotate32, n out of range.")
+	}
+	return (x << n) | (x >> (32 - n))
+}
+
+func uint32ToInt32(x uint32) int32 {
+	return *(*int32)(unsafe.Pointer(&x))
+}
+
+func int32ToUint32(x int32) uint32 {
+	return *(*uint32)(unsafe.Pointer(&x))
+}
+
+// BitCountUint32Pop5 returns the number of 1-bits in x.
+// Source: http://www.hackersdelight.org/hdcodetxt/pop.c.txt (pop5)
+func BitCountUint32Pop5(x uint32) uint {
+	sum := uint32ToInt32(x)
+	for i := 1; i <= 31; i++ {
+		x = rotate32(x, 1)
+		sum = sum + uint32ToInt32(x)
+	}
+	return uint(int32ToUint32(-sum))
+}
+
+// BitCountUint32Pop5a returns the number of 1-bits in x.
+// Source: http://www.hackersdelight.org/hdcodetxt/pop.c.txt (pop5a)
+func BitCountUint32Pop5a(x uint32) uint {
+	sum := uint32ToInt32(x)
+	for x != 0 {
+		x = x >> 1
+		sum = sum - uint32ToInt32(x)
+	}
+	return uint(int32ToUint32(sum))
+}
+
+var table = [...]uint{
+	0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
+	1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+	1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+	2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+
+	1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+	2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+	2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+	3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+
+	1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+	2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+	2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+	3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+
+	2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+	3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+	3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+	4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8,
+}
+
+// BitCountUint32Pop6 returns the number of 1-bits in x.
+// Source: http://www.hackersdelight.org/hdcodetxt/pop.c.txt (pop5a)
+func BitCountUint32Pop6(x uint32) uint {
+	return table[x&0xff] +
+		table[(x>>8)&0xff] +
+		table[(x>>16)&0xff] +
+		table[(x>>24)]
 }

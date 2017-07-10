bc4854e8ee7c60e10cc57b82c22ae551913a0ce6
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2017-07-11 00:55:56 +0800

    Renamed the constants

--- common/limits.go
+++ common/limits.go
@@ -1,10 +1,11 @@
 package common
 
+// Integer limit values.
 const (
-	MaxUint              = ^uint(0)
-	MaxInt               = int(MaxUint >> 1)
-	MinInt               = ^MaxInt
-	LogSizeOfUintInBytes = MaxUint>>8&1 + MaxUint>>16&1 + MaxUint>>32&1
-	SizeOfUintInBytes    = 1 << LogSizeOfUintInBytes
-	SizeOfUintInBits     = SizeOfUintInBytes << 3
+	MaxUint             = ^uint(0)
+	MaxInt              = int(MaxUint >> 1)
+	MinInt              = ^MaxInt
+	LogSizeOfIntInBytes = MaxUint>>8&1 + MaxUint>>16&1 + MaxUint>>32&1
+	SizeOfIntInBytes    = 1 << LogSizeOfIntInBytes
+	SizeOfIntInBits     = SizeOfIntInBytes << 3
 )

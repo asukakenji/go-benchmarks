6cec8de5ebcc3b4d6ee6a3d5607d3ec6d75e370c
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2017-07-11 01:24:14 +0800

    Added Uint32SliceAsByteSlice() and Uint64SliceAsByteSlice()

--- reinterpret/slice.go
+++ reinterpret/slice.go
@@ -8,9 +8,10 @@ import (
 )
 
 // IntSliceAsByteSlice reinterprets a []int as a []byte.
-// Since the returned slice does not contribute in reference counting,
+// Since the returned slice does not participate in reference counting,
 // the caller is responsible to keep a reference to the original slice
 // to prevent it from being garbage collected.
+//
 // ID: RS-2-8 (byte = uint8)
 func IntSliceAsByteSlice(x []int) []byte {
 	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
@@ -21,14 +22,43 @@ func IntSliceAsByteSlice(x []int) []byte {
 }
 
 // UintSliceAsByteSlice reinterprets a []uint as a []byte.
-// Since the returned slice does not contribute in reference counting,
+// Since the returned slice does not participate in reference counting,
 // the caller is responsible to keep a reference to the original slice
 // to prevent it from being garbage collected.
+//
 // ID: RS-7-8 (byte = uint8)
 func UintSliceAsByteSlice(x []uint) []byte {
 	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
 		Data: (*reflect.SliceHeader)(unsafe.Pointer(&x)).Data,
 		Len:  len(x) * common.SizeOfIntInBytes,
 		Cap:  cap(x) * common.SizeOfIntInBytes,
+	}))
+}
+
+// Uint32SliceAsByteSlice reinterprets a []uint32 as a []byte.
+// Since the returned slice does not participate in reference counting,
+// the caller is responsible to keep a reference to the original slice
+// to prevent it from being garbage collected.
+//
+// ID: RS-10-8 (byte = uint8)
+func Uint32SliceAsByteSlice(x []uint32) []byte {
+	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
+		Data: (*reflect.SliceHeader)(unsafe.Pointer(&x)).Data,
+		Len:  len(x) << 2,
+		Cap:  cap(x) << 2,
+	}))
+}
+
+// Uint64SliceAsByteSlice reinterprets a []uint64 as a []byte.
+// Since the returned slice does not participate in reference counting,
+// the caller is responsible to keep a reference to the original slice
+// to prevent it from being garbage collected.
+//
+// ID: RS-11-8 (byte = uint8)
+func Uint64SliceAsByteSlice(x []uint64) []byte {
+	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
+		Data: (*reflect.SliceHeader)(unsafe.Pointer(&x)).Data,
+		Len:  len(x) << 3,
+		Cap:  cap(x) << 3,
 	}))
 }
--- reinterpret/slice_test.go
+++ reinterpret/slice_test.go
@@ -12,7 +12,7 @@ func TestIntSliceAsByteSlice(t *testing.T) {
 	expected := []byte{0xef, 0xcd, 0xab, 0x89, 0x67, 0x45, 0x23, 0x01}
 	got := reinterpret.IntSliceAsByteSlice(x)
 	if !reflect.DeepEqual(got, expected) {
-		t.Errorf("IntSliceAsByteSlice(0x%x) = %v, expected %v", x[0], got, expected)
+		t.Errorf("IntSliceAsByteSlice([]int{0x%x}) = %v, expected %v", x[0], got, expected)
 	}
 
 	got[0] = 0x9a
@@ -26,7 +26,7 @@ func TestIntSliceAsByteSlice(t *testing.T) {
 	expected2 := int(0x78563412f0debc9a)
 	got2 := x[0]
 	if got2 != expected2 {
-		t.Errorf("IntSliceAsByteSlice(0x%x) = %v, expected %v", x[0], got, expected)
+		t.Errorf("IntSliceAsByteSlice([]int{0x%x}) = %v, expected %v", x[0], got2, expected2)
 	}
 }
 
@@ -35,7 +35,7 @@ func TestUintSliceAsByteSlice(t *testing.T) {
 	expected := []byte{0xef, 0xcd, 0xab, 0x89, 0x67, 0x45, 0x23, 0x01}
 	got := reinterpret.UintSliceAsByteSlice(x)
 	if !reflect.DeepEqual(got, expected) {
-		t.Errorf("UintSliceAsByteSlice(0x%x) = %v, expected %v", x[0], got, expected)
+		t.Errorf("UintSliceAsByteSlice([]uint{0x%x}) = %v, expected %v", x[0], got, expected)
 	}
 
 	got[0] = 0xab
@@ -49,6 +49,48 @@ func TestUintSliceAsByteSlice(t *testing.T) {
 	expected2 := uint(0x8967452301efcdab)
 	got2 := x[0]
 	if got2 != expected2 {
-		t.Errorf("UintSliceAsByteSlice(0x%x) = %v, expected %v", x[0], got, expected)
+		t.Errorf("UintSliceAsByteSlice([]uint{0x%x}) = %v, expected %v", x[0], got2, expected2)
+	}
+}
+
+func TestUint32SliceAsByteSlice(t *testing.T) {
+	x := []uint32{0x01234567}
+	expected := []byte{0x67, 0x45, 0x23, 0x01}
+	got := reinterpret.Uint32SliceAsByteSlice(x)
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("Uint32SliceAsByteSlice(uint32{0x%x}) = %v, expected %v", x[0], got, expected)
+	}
+
+	got[0] = 0x23
+	got[1] = 0x45
+	got[2] = 0x67
+	got[3] = 0x89
+	expected2 := uint32(0x89674523)
+	got2 := x[0]
+	if got2 != expected2 {
+		t.Errorf("Uint32SliceAsByteSlice(uint32{0x%x}) = %v, expected %v", x[0], got2, expected2)
+	}
+}
+
+func TestUint64SliceAsByteSlice(t *testing.T) {
+	x := []uint64{0x0123456789abcdef}
+	expected := []byte{0xef, 0xcd, 0xab, 0x89, 0x67, 0x45, 0x23, 0x01}
+	got := reinterpret.Uint64SliceAsByteSlice(x)
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("Uint64SliceAsByteSlice(uint64{0x%x}) = %v, expected %v", x[0], got, expected)
+	}
+
+	got[0] = 0xab
+	got[1] = 0xcd
+	got[2] = 0xef
+	got[3] = 0x01
+	got[4] = 0x23
+	got[5] = 0x45
+	got[6] = 0x67
+	got[7] = 0x89
+	expected2 := uint64(0x8967452301efcdab)
+	got2 := x[0]
+	if got2 != expected2 {
+		t.Errorf("Uint64SliceAsByteSlice(uint64{0x%x}) = %v, expected %v", x[0], got2, expected2)
 	}
 }

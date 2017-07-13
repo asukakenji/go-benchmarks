f12af4adb178f1653ced4a9eb5b3c7411a97820e
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2017-07-13 16:45:11 +0800

    Renamed the function; Added more implementations

--- combination/combination.go
+++ combination/combination.go
@@ -1,8 +1,8 @@
 package combination
 
-func MergeEachPairOfElementsInSetByAddition(set []uint, f func([]uint)) {
+func MergeEachPairOfElementsInMultisetByAddition0(set []uint, f func([]uint)) {
 	n := len(set)
-	for i := 0; i < n-1; i++ {
+	for i, i2 := 0, n-1; i < i2; i++ {
 		set[0], set[i] = set[i], set[0]
 		for j := i + 1; j < n; j++ {
 			set[j] += set[0]
@@ -10,5 +10,66 @@ func MergeEachPairOfElementsInSetByAddition(set []uint, f func([]uint)) {
 			set[j] -= set[0]
 		}
 		set[0], set[i] = set[i], set[0]
+	}
+}
+
+func MergeEachPairOfElementsInMultisetByAddition1(set []uint, f func([]uint)) {
+	n := len(set)
+	for i, i2 := 0, n-1; i < i2; i++ {
+		t1 := set[0]
+		set[0] = set[i]
+		set[i] = t1
+		for j := i + 1; j < n; j++ {
+			set[j] += set[0]
+			f(set[1:])
+			set[j] -= set[0]
+		}
+		set[i] = set[0]
+		set[0] = t1
+	}
+}
+
+func MergeEachPairOfElementsInMultisetByAddition2Alt1(set []uint, f func([]uint)) {
+	n := len(set)
+	for i := 0; i < n-1; i++ {
+		set[0], set[i] = set[i], set[0]
+		for j := i + 1; j < n; j++ {
+			t2 := set[j]
+			set[j] += set[0]
+			f(set[1:])
+			set[j] = t2
+		}
+		set[0], set[i] = set[i], set[0]
+	}
+}
+
+func MergeEachPairOfElementsInMultisetByAddition2Alt2(set []uint, f func([]uint)) {
+	n := len(set)
+	for i, i2 := 0, n-1; i < i2; i++ {
+		set[0], set[i] = set[i], set[0]
+		for j := i + 1; j < n; j++ {
+			t2 := set[j]
+			set[j] += set[0]
+			f(set[1:])
+			set[j] = t2
+		}
+		set[0], set[i] = set[i], set[0]
+	}
+}
+
+func MergeEachPairOfElementsInMultisetByAddition3(set []uint, f func([]uint)) {
+	n := len(set)
+	for i, i2 := 0, n-1; i < i2; i++ {
+		t1 := set[0]
+		set[0] = set[i]
+		set[i] = t1
+		for j := i + 1; j < n; j++ {
+			t2 := set[j]
+			set[j] += set[0]
+			f(set[1:])
+			set[j] = t2
+		}
+		set[i] = set[0]
+		set[0] = t1
 	}
 }
--- combination/combination_test.go
+++ combination/combination_test.go
@@ -20,7 +20,17 @@ func (c *Collector) Collect(x []uint) {
 	*c = append(*c, xCopy)
 }
 
-func TestMergeEachPairOfElementsInSetByAddition(t *testing.T) {
+func TestMergeEachPairOfElementsInMultisetByAddition(t *testing.T) {
+	implementations := []struct {
+		name string
+		f    func([]uint, func([]uint))
+	}{
+		{"MergeEachPairOfElementsInMultisetByAddition0", combination.MergeEachPairOfElementsInMultisetByAddition0},
+		{"MergeEachPairOfElementsInMultisetByAddition1", combination.MergeEachPairOfElementsInMultisetByAddition1},
+		{"MergeEachPairOfElementsInMultisetByAddition2Alt1", combination.MergeEachPairOfElementsInMultisetByAddition2Alt1},
+		{"MergeEachPairOfElementsInMultisetByAddition2Alt2", combination.MergeEachPairOfElementsInMultisetByAddition2Alt2},
+		{"MergeEachPairOfElementsInMultisetByAddition3", combination.MergeEachPairOfElementsInMultisetByAddition3},
+	}
 	cases := []struct {
 		set      []uint
 		expected [][]uint
@@ -74,12 +84,62 @@ func TestMergeEachPairOfElementsInSetByAddition(t *testing.T) {
 			},
 		},
 	}
-	for _, c := range cases {
-		collector := NewCollector()
-		combination.MergeEachPairOfElementsInSetByAddition(c.set, collector.Collect)
-		got := ([][]uint)(*collector)
-		if !reflect.DeepEqual(got, c.expected) {
-			t.Errorf("MergeEachPairOfElementsInSetByAddition(%v, collector.Collect) = %v, expected %v", c.set, got, c.expected)
+	for _, impl := range implementations {
+		for _, c := range cases {
+			collector := NewCollector()
+			impl.f(c.set, collector.Collect)
+			got := ([][]uint)(*collector)
+			if !reflect.DeepEqual(got, c.expected) {
+				t.Errorf("%s(%v, collector.Collect) = %v, expected %v", impl.name, c.set, got, c.expected)
+			}
 		}
 	}
+}
+
+var benchmarkCases = [][]uint{
+	{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+	{0, 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9, 0},
+	{0, 0, 1, 1, 0, 1, 2, 0, 1, 3, 0, 1, 4, 0, 1, 5, 0, 1, 6, 0, 1, 7, 0, 1, 8, 0, 1, 9, 0, 1},
+	{0, 0, 1, 9, 1, 0, 1, 9, 2, 0, 1, 9, 3, 0, 1, 9, 4, 0, 1, 9, 5, 0, 1, 9, 6, 0, 1, 9, 7, 0, 1, 9, 8, 0, 1, 9, 9, 0, 1, 9},
+}
+
+func dummy(_ []uint) {
+}
+
+// BenchmarkMergeEachPairOfElementsInMultisetByAddition0-8       	 1000000	      1195 ns/op
+// BenchmarkMergeEachPairOfElementsInMultisetByAddition1-8       	 1000000	      1226 ns/op
+// BenchmarkMergeEachPairOfElementsInMultisetByAddition2Alt1-8   	 1000000	      1157 ns/op
+// BenchmarkMergeEachPairOfElementsInMultisetByAddition2Alt2-8   	 1000000	      1130 ns/op <- Best
+// BenchmarkMergeEachPairOfElementsInMultisetByAddition3-8       	 1000000	      1254 ns/op
+
+func benchmarkMergeEachPairOfElementsInMultisetByAddition(b *testing.B, impl func([]uint, func([]uint))) {
+	n := len(benchmarkCases)
+	index := 0
+	for i, count := 0, b.N; i < count; i++ {
+		impl(benchmarkCases[index], dummy)
+		index++
+		if index == n {
+			index = 0
+		}
+	}
+}
+
+func BenchmarkMergeEachPairOfElementsInMultisetByAddition0(b *testing.B) {
+	benchmarkMergeEachPairOfElementsInMultisetByAddition(b, combination.MergeEachPairOfElementsInMultisetByAddition0)
+}
+
+func BenchmarkMergeEachPairOfElementsInMultisetByAddition1(b *testing.B) {
+	benchmarkMergeEachPairOfElementsInMultisetByAddition(b, combination.MergeEachPairOfElementsInMultisetByAddition1)
+}
+
+func BenchmarkMergeEachPairOfElementsInMultisetByAddition2Alt1(b *testing.B) {
+	benchmarkMergeEachPairOfElementsInMultisetByAddition(b, combination.MergeEachPairOfElementsInMultisetByAddition2Alt1)
+}
+
+func BenchmarkMergeEachPairOfElementsInMultisetByAddition2Alt2(b *testing.B) {
+	benchmarkMergeEachPairOfElementsInMultisetByAddition(b, combination.MergeEachPairOfElementsInMultisetByAddition2Alt2)
+}
+
+func BenchmarkMergeEachPairOfElementsInMultisetByAddition3(b *testing.B) {
+	benchmarkMergeEachPairOfElementsInMultisetByAddition(b, combination.MergeEachPairOfElementsInMultisetByAddition3)
 }

0614deef1cd7776cc3a1ab6520611e498f7fac0c
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2017-08-28 22:58:14 +0800

    Added new functions

--- common/reinterpret/int.go
+++ common/reinterpret/int.go
@@ -8,6 +8,18 @@ func IntAsUint(x int) uint {
 	return *(*uint)(unsafe.Pointer(&x))
 }
 
+// Int8AsUint8 reinterprets an int8 value bit-by-bit as an uint8 value.
+// ID: R-3-8
+func Int8AsUint8(x int8) uint8 {
+	return *(*uint8)(unsafe.Pointer(&x))
+}
+
+// Int16AsUint16 reinterprets an int16 value bit-by-bit as an uint16 value.
+// ID: R-4-9
+func Int16AsUint16(x int16) uint16 {
+	return *(*uint16)(unsafe.Pointer(&x))
+}
+
 // Int32AsUint32 reinterprets an int32 value bit-by-bit as an uint32 value.
 // ID: R-5-10
 func Int32AsUint32(x int32) uint32 {
@@ -24,6 +36,18 @@ func Int64AsUint64(x int64) uint64 {
 // ID: R-7-2
 func UintAsInt(x uint) int {
 	return *(*int)(unsafe.Pointer(&x))
+}
+
+// Uint8AsInt8 reinterprets an uint8 value bit-by-bit as an int8 value.
+// ID: R-8-3
+func Uint8AsInt8(x uint8) int8 {
+	return *(*int8)(unsafe.Pointer(&x))
+}
+
+// Uint16AsInt16 reinterprets an uint16 value bit-by-bit as an int16 value.
+// ID: R-9-4
+func Uint16AsInt16(x uint16) int16 {
+	return *(*int16)(unsafe.Pointer(&x))
 }
 
 // Uint32AsInt32 reinterprets an uint32 value bit-by-bit as an int32 value.
--- common/reinterpret/slice.go
+++ common/reinterpret/slice.go
@@ -21,6 +21,62 @@ func IntSliceAsByteSlice(x []int) []byte {
 	}))
 }
 
+// Int8SliceAsByteSlice reinterprets a []int8 as a []byte.
+// Since the returned slice does not participate in reference counting,
+// the caller is responsible to keep a reference to the original slice
+// to prevent it from being garbage collected.
+//
+// ID: RS-3-8 (byte = uint8)
+func Int8SliceAsByteSlice(x []int8) []byte {
+	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
+		Data: (*reflect.SliceHeader)(unsafe.Pointer(&x)).Data,
+		Len:  len(x),
+		Cap:  cap(x),
+	}))
+}
+
+// Int16SliceAsByteSlice reinterprets a []int16 as a []byte.
+// Since the returned slice does not participate in reference counting,
+// the caller is responsible to keep a reference to the original slice
+// to prevent it from being garbage collected.
+//
+// ID: RS-4-8 (byte = uint8)
+func Int16SliceAsByteSlice(x []int16) []byte {
+	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
+		Data: (*reflect.SliceHeader)(unsafe.Pointer(&x)).Data,
+		Len:  len(x) << 1,
+		Cap:  cap(x) << 1,
+	}))
+}
+
+// Int32SliceAsByteSlice reinterprets a []int32 as a []byte.
+// Since the returned slice does not participate in reference counting,
+// the caller is responsible to keep a reference to the original slice
+// to prevent it from being garbage collected.
+//
+// ID: RS-5-8 (byte = uint8)
+func Int32SliceAsByteSlice(x []int32) []byte {
+	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
+		Data: (*reflect.SliceHeader)(unsafe.Pointer(&x)).Data,
+		Len:  len(x) << 2,
+		Cap:  cap(x) << 2,
+	}))
+}
+
+// Int64SliceAsByteSlice reinterprets a []int64 as a []byte.
+// Since the returned slice does not participate in reference counting,
+// the caller is responsible to keep a reference to the original slice
+// to prevent it from being garbage collected.
+//
+// ID: RS-6-8 (byte = uint8)
+func Int64SliceAsByteSlice(x []int64) []byte {
+	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
+		Data: (*reflect.SliceHeader)(unsafe.Pointer(&x)).Data,
+		Len:  len(x) << 3,
+		Cap:  cap(x) << 3,
+	}))
+}
+
 // UintSliceAsByteSlice reinterprets a []uint as a []byte.
 // Since the returned slice does not participate in reference counting,
 // the caller is responsible to keep a reference to the original slice
@@ -32,6 +88,28 @@ func UintSliceAsByteSlice(x []uint) []byte {
 		Data: (*reflect.SliceHeader)(unsafe.Pointer(&x)).Data,
 		Len:  len(x) * common.SizeOfIntInBytes,
 		Cap:  cap(x) * common.SizeOfIntInBytes,
+	}))
+}
+
+// Uint8SliceAsByteSlice reinterprets a []uint8 as a []byte.
+// Since byte is an alias of uint8, x is returned.
+//
+// ID: RS-8-8 (byte = uint8)
+func Uint8SliceAsByteSlice(x []uint8) []byte {
+	return x
+}
+
+// Uint16SliceAsByteSlice reinterprets a []uint16 as a []byte.
+// Since the returned slice does not participate in reference counting,
+// the caller is responsible to keep a reference to the original slice
+// to prevent it from being garbage collected.
+//
+// ID: RS-9-8 (byte = uint8)
+func Uint16SliceAsByteSlice(x []uint16) []byte {
+	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
+		Data: (*reflect.SliceHeader)(unsafe.Pointer(&x)).Data,
+		Len:  len(x) << 2,
+		Cap:  cap(x) << 2,
 	}))
 }
 

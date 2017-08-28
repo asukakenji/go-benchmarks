6d8f2d58a0f0faa962f65990860b59b0ec85aeaa
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2017-08-28 12:11:16 +0800

    Added stubs for OnesCount; Fixed compilation error (no package)

---
+++ math/bits/bits.go
@@ -0,0 +1,26 @@
+package bits
+
+// OnesCount returns the number of one bits ("population count") in x.
+func OnesCount(x uint) int {
+	return 0
+}
+
+// OnesCount8 returns the number of one bits ("population count") in x.
+func OnesCount8(x uint8) int {
+	return 0
+}
+
+// OnesCount16 returns the number of one bits ("population count") in x.
+func OnesCount16(x uint16) int {
+	return 0
+}
+
+// OnesCount32 returns the number of one bits ("population count") in x.
+func OnesCount32(x uint32) int {
+	return 0
+}
+
+// OnesCount64 returns the number of one bits ("population count") in x.
+func OnesCount64(x uint64) int {
+	return 0
+}

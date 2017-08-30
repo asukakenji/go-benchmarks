17691671d30392f2ee577f667da3f9c0e5def69f
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2017-08-30 15:50:33 +0800

    Renamed variables

--- common/benchmark/consumer.go
+++ common/benchmark/consumer.go
@@ -5,98 +5,98 @@ import "testing"
 // BoolConsumer benchmarks a function with the signature:
 //     func(bool)
 // ID: B-0-1
-func BoolConsumer(b *testing.B, genFunc func() bool, f func(bool)) {
+func BoolConsumer(b *testing.B, supplier func() bool, consumer func(bool)) {
 	for i, count := 0, b.N; i < count; i++ {
-		f(genFunc())
+		consumer(supplier())
 	}
 }
 
 // IntConsumer benchmarks a function with the signature:
 //     func(int)
 // ID: B-0-2
-func IntConsumer(b *testing.B, genFunc func() int, f func(int)) {
+func IntConsumer(b *testing.B, supplier func() int, consumer func(int)) {
 	for i, count := 0, b.N; i < count; i++ {
-		f(genFunc())
+		consumer(supplier())
 	}
 }
 
 // Int8Consumer benchmarks a function with the signature:
 //     func(int8)
 // ID: B-0-3
-func Int8Consumer(b *testing.B, genFunc func() int8, f func(int8)) {
+func Int8Consumer(b *testing.B, supplier func() int8, consumer func(int8)) {
 	for i, count := 0, b.N; i < count; i++ {
-		f(genFunc())
+		consumer(supplier())
 	}
 }
 
 // Int16Consumer benchmarks a function with the signature:
 //     func(int16)
 // ID: B-0-4
-func Int16Consumer(b *testing.B, genFunc func() int16, f func(int16)) {
+func Int16Consumer(b *testing.B, supplier func() int16, consumer func(int16)) {
 	for i, count := 0, b.N; i < count; i++ {
-		f(genFunc())
+		consumer(supplier())
 	}
 }
 
 // Int32Consumer benchmarks a function with the signature:
 //     func(int32)
 // ID: B-0-5
-func Int32Consumer(b *testing.B, genFunc func() int32, f func(int32)) {
+func Int32Consumer(b *testing.B, supplier func() int32, consumer func(int32)) {
 	for i, count := 0, b.N; i < count; i++ {
-		f(genFunc())
+		consumer(supplier())
 	}
 }
 
 // Int64Consumer benchmarks a function with the signature:
 //     func(int64)
 // ID: B-0-6
-func Int64Consumer(b *testing.B, genFunc func() int64, f func(int64)) {
+func Int64Consumer(b *testing.B, supplier func() int64, consumer func(int64)) {
 	for i, count := 0, b.N; i < count; i++ {
-		f(genFunc())
+		consumer(supplier())
 	}
 }
 
 // UintConsumer benchmarks a function with the signature:
 //     func(uint)
 // ID: B-0-7
-func UintConsumer(b *testing.B, genFunc func() uint, f func(uint)) {
+func UintConsumer(b *testing.B, supplier func() uint, consumer func(uint)) {
 	for i, count := 0, b.N; i < count; i++ {
-		f(genFunc())
+		consumer(supplier())
 	}
 }
 
 // Uint8Consumer benchmarks a function with the signature:
 //     func(uint8)
 // ID: B-0-8
-func Uint8Consumer(b *testing.B, genFunc func() uint8, f func(uint8)) {
+func Uint8Consumer(b *testing.B, supplier func() uint8, consumer func(uint8)) {
 	for i, count := 0, b.N; i < count; i++ {
-		f(genFunc())
+		consumer(supplier())
 	}
 }
 
 // Uint16Consumer benchmarks a function with the signature:
 //     func(uint16)
 // ID: B-0-9
-func Uint16Consumer(b *testing.B, genFunc func() uint16, f func(uint16)) {
+func Uint16Consumer(b *testing.B, supplier func() uint16, consumer func(uint16)) {
 	for i, count := 0, b.N; i < count; i++ {
-		f(genFunc())
+		consumer(supplier())
 	}
 }
 
 // Uint32Consumer benchmarks a function with the signature:
 //     func(uint32)
 // ID: B-0-10
-func Uint32Consumer(b *testing.B, genFunc func() uint32, f func(uint32)) {
+func Uint32Consumer(b *testing.B, supplier func() uint32, consumer func(uint32)) {
 	for i, count := 0, b.N; i < count; i++ {
-		f(genFunc())
+		consumer(supplier())
 	}
 }
 
 // Uint64Consumer benchmarks a function with the signature:
 //     func(uint64)
 // ID: B-0-11
-func Uint64Consumer(b *testing.B, genFunc func() uint64, f func(uint64)) {
+func Uint64Consumer(b *testing.B, supplier func() uint64, consumer func(uint64)) {
 	for i, count := 0, b.N; i < count; i++ {
-		f(genFunc())
+		consumer(supplier())
 	}
 }
--- common/benchmark/runnable.go
+++ common/benchmark/runnable.go
@@ -5,8 +5,8 @@ import "testing"
 // Runnable benchmarks a function with the signature:
 //     func()
 // ID: B-0-0
-func Runnable(b *testing.B, f func()) {
+func Runnable(b *testing.B, runnable func()) {
 	for i, count := 0, b.N; i < count; i++ {
-		f()
+		runnable()
 	}
 }

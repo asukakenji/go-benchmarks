a20c8316289f09e4fef8f89a784eb64bffed9768
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2022-02-11 06:41:10 +0800

    Fixed bugs

--- math/bits/impl/leadingzeros/stdlib/bits.go
+++ math/bits/impl/leadingzeros/stdlib/bits.go
@@ -5,7 +5,7 @@ import "github.com/asukakenji/go-benchmarks"
 // --- LeadingZeros ---
 
 // LeadingZeros returns the number of leading zero bits in x; the result is the size of uint in bits for x == 0.
-func LeadingZeros(x uint) int { return int(benchmarks.LogSizeOfUintInBytes) - Len(x) }
+func LeadingZeros(x uint) int { return int(benchmarks.SizeOfUintInBits) - Len(x) }
 
 // LeadingZeros8 returns the number of leading zero bits in x; the result is 8 for x == 0.
 func LeadingZeros8(x uint8) int { return 8 - Len8(x) }
@@ -20,13 +20,13 @@ func LeadingZeros32(x uint32) int { return 32 - Len32(x) }
 func LeadingZeros64(x uint64) int { return 64 - Len64(x) }
 
 // LeadingZerosPtr returns the number of leading zero bits in x; the result is the size of uintptr in bits for x == 0.
-func LeadingZerosPtr(x uintptr) int { return int(benchmarks.LogSizeOfUintInBytes) - LenPtr(x) }
+func LeadingZerosPtr(x uintptr) int { return benchmarks.SizeOf[uintptr]() - LenPtr(x) }
 
 // --- Len ---
 
 // Len returns the minimum number of bits required to represent x; the result is 0 for x == 0.
 func Len(x uint) int {
-	if benchmarks.LogSizeOfUintInBytes == 32 {
+	if benchmarks.SizeOfUintInBits == 32 {
 		return Len32(uint32(x))
 	}
 	return Len64(uint64(x))
@@ -78,7 +78,7 @@ func Len64(x uint64) (n int) {
 
 // LenPtr returns the minimum number of bits required to represent x; the result is 0 for x == 0.
 func LenPtr(x uintptr) int {
-	if benchmarks.LogSizeOfUintInBytes == 32 {
+	if benchmarks.SizeOf[uintptr]() == 32 {
 		return Len32(uint32(x))
 	}
 	return Len64(uint64(x))
